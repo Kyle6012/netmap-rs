70b0736505d569feb7ec987cae2811c91d4b4c43
Meshack Bahati <102649537+Kyle6012@users.noreply.github.com>
2025-05-05 21:38:03 +0300

    #shared library confirmation

--- netmap-min-sys/build.rs
+++ netmap-min-sys/build.rs
@@ -1,13 +1,26 @@
-use std::env;
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
 fn main() {
+    // Always re-run when NETMAP_LOCATION is updated
     println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-env-changed=NETMAP_LOCATION");
+    println!("cargo:rerun-if-env-changed=DISABLE_NETMAP_KERNEL");
 
-    // tell cargo to look for shraed libraries in the following directories 
-    println!("cargo:rust-link-search=/usr/local/lib");
-    println!("cargo:rustc-link-lib=netmap");
+    // If we're in CI / userspace mode, skip any attempt to link or build the
+    // in-kernel Netmap module.
+    if env::var("DISABLE_NETMAP_KERNEL").is_ok() {
+        println!("cargo:warning=DISABLE_NETMAP_KERNEL set; skipping Netmap kernel integration");
+    } else {
+        // Where did the user install netmap? Defaults to /usr/local
+        let install_dir = env::var("NETMAP_LOCATION").unwrap_or_else(|_| "/usr/local".into());
+        println!("cargo:warning=Linking against Netmap in: {}", install_dir);
 
+        // Tell Cargo / rustc where to find the shared library
+        println!("cargo:rustc-link-search=native={}/lib", install_dir);
+        println!("cargo:rustc-link-lib=dylib=netmap");
+    }
+
+    // Now generate the Rust bindings via bindgen:
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -16,13 +29,13 @@ fn main() {
         .allowlist_var("NETMAP_.*")
         .size_t_is_usize(true)
         .default_enum_style(bindgen::EnumVariation::Rust {
-            non_exhaustive:false,
+            non_exhaustive: false,
         })
         .generate()
-        .expect("Unable to generate bindings");
+        .expect("Unable to generate bindings with bindgen");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings");
+        .expect("Couldn't write bindings to file");
 }
